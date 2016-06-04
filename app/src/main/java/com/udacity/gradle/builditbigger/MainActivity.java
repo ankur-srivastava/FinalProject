6798877b1baed516bd5f2cb14a28d8ce0f5c398a
Ankur Srivastava <ankurlkw@gmail.com>
2016-06-04 10:40:46 +0530

    App is able to invoke the backend

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,8 +1,11 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v4.util.Pair;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,9 +13,18 @@ import android.widget.Toast;
 
 import com.edocent.displayjokes.DisplayJoke;
 import com.example.JokeRepo;
+import com.example.ankursrivastava.myapplication.backend.myApi.MyApi;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+
+import java.io.IOException;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    private static final String NAME = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,10 +56,55 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view){
+        /*
         Intent intent = new Intent(this, DisplayJoke.class);
         intent.putExtra(DisplayJoke.JOKE_INTENT_KEY, JokeRepo.getJoke());
         startActivity(intent);
+        */
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Ankur"));
     }
 
 
+    //GCE Module Test
+    class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+        private MyApi myApiService = null;
+        private Context context;
+
+        @Override
+        protected String doInBackground(Pair<Context, String>... params) {
+            Log.v(NAME, "In doInBackground");
+            if(myApiService == null) {  // Only do this once
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        // options for running against local devappserver
+                        // - 10.0.2.2 is localhost's IP address in Android emulator
+                        // - turn off compression when running against local devappserver
+                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+                // end options for devappserver
+
+                myApiService = builder.build();
+            }
+
+            context = params[0].first;
+            String name = params[0].second;
+
+            try {
+                return myApiService.sayHi(name).execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.v(NAME, "In onPostExecute "+result);
+            Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+        }
+    }
 }
